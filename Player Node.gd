4be189dd469a98cd7c20e0fb2fe4693c8588b0a7
Dmytro Cheva <dmytro.cheva@gmail.com>
2024-08-06 01:31:46 +0300

    Change AnimatedSprite to AnimationPlayer and sprites to frameset

--- Player Node.gd
+++ Player Node.gd
@@ -1,27 +1,31 @@
 extends KinematicBody2D
 
+onready var _animation_player = $AnimationPlayer
+onready var _sprite = $Sprite
+
 var velocity = Vector2(0,0)
+
 var speed = 300
 var jump_speed = -1000
 var gravity = 2000
 
 func _physics_process(_delta):
 	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y= jump_speed
-		$Sprite.play("Jump-up")
+		velocity.y = jump_speed
+		_animation_player.play("Jump")
 	elif Input.is_action_pressed("left_arrow"):
-		$Sprite.play("Run-right")
+		_animation_player.play("Run")
 		$Sprite.flip_h = true
 		velocity.x = -speed
 	elif Input.is_action_pressed("right_arrow"):
-		$Sprite.play("Run-right")
+		_animation_player.play("Run")
 		$Sprite.flip_h = false
 		velocity.x = speed
 	else:
-		 $Sprite.play("Idle")
+		  _animation_player.play("Idle")
 		
 	if Input.is_action_pressed("jump") or not is_on_floor():
-		$Sprite.play("Jump-up")
+		_animation_player.play("Jump")
 		
 	velocity.y = velocity.y + gravity * (_delta)
 	velocity.x = lerp(velocity.x, 0, _delta * 4)
