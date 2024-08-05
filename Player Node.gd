48113e44b56dc5200e52023060e7879d92e9d586
Dmytro Cheva <dmytro.cheva@gmail.com>
2024-08-05 14:52:47 +0300

    Create Jump and prepare to Creating Animations Using a Sprite Sheet

--- Player Node.gd
+++ Player Node.gd
@@ -1,29 +1,25 @@
 extends KinematicBody2D
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
 var velocity = Vector2(0,0)
 var speed = 300
+var jump_speed = 1000
+var gravity = 2000
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _physics_process(delta):
-	if Input.is_action_pressed("left_arrow"):
+func _physics_process(_delta):
+		
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y= -jump_speed
+	if Input.is_action_pressed("jump"):
+		$Sprite.play("Jump-up")
+	elif Input.is_action_pressed("left_arrow"):
 		$Sprite.play("Roll-left")
 		velocity.x = -speed
 	elif Input.is_action_pressed("right_arrow"):
 		$Sprite.play("Roll-right")
 		velocity.x = speed
-	else:
-		$Sprite.play("Idle")
-	
-	move_and_slide(velocity)
-	
-	velocity.x = lerp(velocity.x, 0, delta*4)
+	elif velocity == Vector2(0,0):
+		 $Sprite.play("Idle")
+		
+	velocity.y = velocity.y + gravity * (_delta)
+	velocity.x = lerp(velocity.x, 0, _delta * 4)
+	move_and_slide(velocity, Vector2.UP)
