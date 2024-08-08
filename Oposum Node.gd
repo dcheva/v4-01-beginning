7a5b051f3481227300b8bc03d61acc5213e5ba1b
Dmytro Cheva <dmytro.cheva@gmail.com>
2024-08-08 15:30:51 +0300

    Oposum Walkin!!!

--- Oposum Node.gd
+++ Oposum Node.gd
@@ -1,16 +1,20 @@
 extends KinematicBody2D
 
 var velocity = Vector2(0,0)
-var speed = 200
+var speed = 100
 var direction = -1
 
 func _physics_process(_delta):
 	velocity.x = speed * direction
-	
+
 	if direction == 1:
 		$Sprite.flip_h = true
 	else:
 		$Sprite.flip_h = false
-		
+
 	$AnimationPlayer.play("Oposun walk")
 	move_and_slide(velocity)
+
+	if $RayCast2D.is_colliding() == false:
+		direction = direction * -1
+		$RayCast2D.position.x *= -1
