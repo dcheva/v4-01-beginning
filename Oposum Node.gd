12cf74a7463b899b8c6363da5066de6ea2f3d7d1
Dmytro Cheva <dmytro.cheva@gmail.com>
2024-08-08 15:57:34 +0300

    Lives and bonces. Start Collision Layer and Collision Mask

--- Oposum Node.gd
+++ Oposum Node.gd
@@ -3,6 +3,7 @@ extends KinematicBody2D
 var velocity = Vector2(0,0)
 var speed = 100
 var direction = -1
+var lives = 3
 
 func _physics_process(_delta):
 	velocity.x = speed * direction
@@ -18,3 +19,10 @@ func _physics_process(_delta):
 	if $RayCast2D.is_colliding() == false:
 		direction = direction * -1
 		$RayCast2D.position.x *= -1
+
+func _on_CollisionChecker_body_entered(_body):
+	print("Bounced enemy")
+	lives -= 1
+	prints("Lives: ", lives)
+	if(lives == 0):      
+		get_tree().change_scene("res://Scene.tscn")
